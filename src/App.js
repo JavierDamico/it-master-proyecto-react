e70a374cf7a81c51fefe129429126b21086fc0fb
JavierDamico <javierdamico82@gmail.com>
2021-04-23 16:55:59 -0300

    pasando parte del proyecto de clase a react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+import "./index.css";
+import hamburguesa from "./hamburguesa.svg";
+import logo from "./logo.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,40 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div id="first-nav">
+        <a href="#footer">Sign in</a> or
+        <a href="#footer">create an account</a>
+      </div>
+      <div id="second-nav">
+        <img className="logo" src={logo} alt="Logo Google" />
+        <div className="second-nav-menu">
+          <div className="second-nav-hamburger">
+            <img src={hamburguesa} alt="" />
+          </div>
+
+          <div className="second-nav-links"></div>
+        </div>
+      </div>
+      <div id="banner">
+        <div className="overlay">
+          <h1>Lorem Ipsum</h1>
+          <button>Learn more</button>
+        </div>
+      </div>
+      <div id="items"></div>
+      <div className="paginacion">Ver mas productos</div>
+      <div id="footer">
+        <p>Suscribite al newsletter</p>
+        <form action="" method="post">
+          <input type="text" />
+          <button>Enviar</button>
+        </form>
+      </div>
+      <div className="cookie">
+        En esta pagina utilizamos cookies para brindarle un mejor servicio.
+        <a href="#">Aceptar</a>
+      </div>
     </div>
   );
 }
